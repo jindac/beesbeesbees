292faf8af1cd9da2677a9d93a30f471d10524f0b
Derrick Chan <dachanman91@gmail.com>
2016-12-19 11:42:33 -0800

    inherite food and eat properties from Grub.

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,13 @@
 var RetiredForagerBee = function() {
+  Bee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
+RetiredForagerBee.prototype = Object.create(Bee.prototype);
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+
 
