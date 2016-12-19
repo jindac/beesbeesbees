9c18ad86640ec3945a56c96523b7cadcc65147d3
Derrick Chan <dachanman91@gmail.com>
2016-12-19 11:45:21 -0800

    RetiredForagerBee has gamble method and correct constructor binding.

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,13 +1,17 @@
 var RetiredForagerBee = function() {
-  Bee.call(this);
+  ForagerBee.call(this);
   this.age = 40;
   this.job = 'gamble';
   this.canFly = false;
   this.color = 'grey';
 };
-RetiredForagerBee.prototype = Object.create(Bee.prototype);
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
 RetiredForagerBee.prototype.forage = function() {
   return 'I am too old, let me play cards instead';
-}
+};
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure)
+};
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
 
 
