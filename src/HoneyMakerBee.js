46da20d3337c0425d0fd0bf90eeab161957ddaed
Derrick Chan <dachanman91@gmail.com>
2016-12-19 11:24:33 -0800

    HoneyMakerBee has honeyPot property and makeHoney method which adds 1 to honeyPot

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -2,5 +2,9 @@ var HoneyMakerBee = function() {
   Bee.call(this);
   this.age = 10;
   this.job = 'make honey';
+  this.honeyPot = 0;
 };
 HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
