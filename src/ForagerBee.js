b069476559931abddf77baa65f5c0b21a478bce8
Derrick Chan <dachanman91@gmail.com>
2016-12-19 11:33:20 -0800

    ForagerBee has canFly and treasureChest properties.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -2,5 +2,7 @@ var ForagerBee = function() {
   Bee.call(this);
   this.age = 10;
   this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 ForagerBee.prototype = Object(Bee.prototype);
